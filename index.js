589093dfb5337e1336281d4d18f5221fd054ad33
Vincde <145802178+Vincde@users.noreply.github.com>
2024-10-05 12:41:24 +0200

    %%

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-console.log('hello')
-
-2+222++2+2+2+2+2+
-dfgdfdfsdfsdfsf
-sdfsdfsdfsdfsdfssdfasdfdfasdfsdfsadfsadfasdf
-
-
-
-sfsdfsdfsdfsfsdfsdfsdfsdfsdfsdf
-sdfsdfsdfsdfsdfs
-
-ssssss
+Ciao sono il primo commit main
